ff54c5a800fb1153ee57ca3c9fc46fde7594150d
Jakob Adamsson <49289353+JakobAdamsson@users.noreply.github.com>
2021-12-05 17:37:11 +0100

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -22,7 +22,7 @@ class Linklst:
             print(node.value, node.next.value)
             node = node.next
     
-    def count(self):
+    def count_elems(self):
         node = self.first
         counter = 0
         while node.next != None:
@@ -41,24 +41,34 @@ class Linklst:
         cur_node.next = node
 
 
-    def remove(self, value):
+    def pop(self, value):
         node = Node(value)
         cur_node = self.first
         prev_node = self.prev_node(value)
         while cur_node.next != None:
             if cur_node.value == node.value:
                 prev_node.next = cur_node.next
-                #print('removed', value)
             cur_node = cur_node.next
         cur_node.next = node.next                
                 
+    
+    def peek(self, value):
+        node = Node(value)
+        cur_node = self.first
+        prev_node = self.prev_node(value)
+        while cur_node.next != None:
+            if cur_node.value == node.value:
+                print(f'Properties of chosen value: value -> {node.value}, next node -> {cur_node.next.value}, prev node -> {prev_node.value}')
+                return
+            cur_node = cur_node.next
+        cur_node.next = node
+        return 'Value not found'
                 
     
 lst = [2,3,5,67,98,92,1,2,3,5,7]
 link = Linklst()
 for i in lst:
     link.append(i)
-link.remove(98)
-link.show_link()
+link.peek(2)
 #print(link.prev_node(98))
 #link.show_link()
