fe46c311431d7aec4959c8512f420710340648ea
JakobAdamsson <jakob.adamsson6@gmail.com>
2021-12-05 20:00:24 +0100

    'test'

--- linkedlist.py
+++ linkedlist.py
@@ -65,10 +65,34 @@ class Linklst:
         return 'Value not found'
                 
     
+    def swap(self, value, value2):
+        node = Node(value)
+        node2 = Node(value2)
+        node_prev = self.prev_node(value)
+        node2_prev = self.prev_node(value2)
+        cur_node = self.first
+        while cur_node.next != None:
+            if cur_node.value == node.value:
+                node = node2
+                node.prev = node2_prev
+                node.next = node2.next
+                node2 = cur_node
+                node2.prev = node_prev
+                node2.next = node.next
+                break
+            cur_node = cur_node.next
+        cur_node.next = node
+                
+                
+                
+
 lst = [2,3,5,67,98,92,1,2,3,5,7]
 link = Linklst()
 for i in lst:
     link.append(i)
-link.peek(2)
+link.show_link()
+print('---------')
+link.swap(98,1)
+link.show_link()
 #print(link.prev_node(98))
 #link.show_link()
